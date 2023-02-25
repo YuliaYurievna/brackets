6fd1d5ae2704749f53992d1d6550db76bf3e27e1
Yuliia Poddiacha <104911480+YuliaYurievna@users.noreply.github.com>
2023-02-25 18:11:49 +0200

    feat: add a solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  while (
+    str.length !== 0 &&
+    bracketsConfig.some((el) => str.includes(el.join("")))
+  ) {
+    for (element of bracketsConfig) {
+      const stringToCheck = element.join("");
+      console.log("s", stringToCheck);
+      const regexStr = stringToCheck
+        .replace(/\[\]/, "\\[\\]")
+        .replace(/\{\}/, "\\{\\}")
+        .replace(/\|\|/, "\\|\\|")
+        .replace(/\(\)/, "\\(\\)");
+      console.log("regexStr", regexStr);
+      const regExp = new RegExp(regexStr, "g");
+
+      str = str.replace(regExp, "");
+    }
+  }
+  return str.length === 0;
+};
